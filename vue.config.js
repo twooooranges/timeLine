ac41e687e5900eb79ef9a9330d5c3eac39f1bee0
fengqinmeng <961301827@qq.com>
2025-04-17 17:48:49 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/timeLine/' : '/',
     devServer: {
         https: true, // https:{type:Boolean
         port: 8089 // 此处修改你想要的端口号
